9ab40d58f79da1fe3336ddaaaf2aff77db45a3e3
Rogerio Ribeiro <rogerbsp@gmail.com>
2020-07-05 16:18:48 -0300

    Imprimindo o tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -29,5 +29,12 @@ public class Board {
 		this.colums = colums;
 	}
 	
+
+	public Piece piece(int row, int colum) {
+		return pieces[row][colum];
+	}
 	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColum()];
+	}
 }
