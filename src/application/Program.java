3ec8dc3fa4f28712b1cc68caf0e99f4fe7a15529
Rogerio Ribeiro <rogerbsp@gmail.com>
2020-07-05 15:03:22 -0300

    Implementando Board e Pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,19 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
-	
+
+		
+		Board board= new Board(3, 6);
 
 Position position= new Position(3, 5);
 
 System.out.println(position);
+System.out.println(board);
 	}
 
 }
